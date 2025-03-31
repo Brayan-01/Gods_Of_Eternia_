c7d723e365214c52354ab391bc85237c483ceb3d
Brayan-01 <penagosbrayan931@gmail.com>
2025-03-31 15:22:24 -0500

    hacemos cambios de colores, estilo del header, register y de la verificacion

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,32 @@
 function Footer() {
-    return (
-      <footer className="footer">
-        <div className="footer-container">
-          <div>
-            <h4 className="footer-title">Redes Sociales</h4>
-            <div className="footer-icons">
-              <a href="#"><i className="ri-twitter-x-line"></i></a>
-              <a href="#"><i className="ri-discord-line"></i></a>
-              <a href="#"><i className="ri-youtube-line"></i></a>
-            </div>
-          </div>
-          <div>
-            <h4 className="footer-title">Enlaces Útiles</h4>
-            <ul className="footer-links">
-              <li><a href="#">Soporte</a></li>
-              <li><a href="#">Guía del Juego</a></li>
-            </ul>
-          </div>
-          <div>
-            <h4 className="footer-title">Legal</h4>
-            <ul className="footer-links">
-              <li><a href="#">Términos de Servicio</a></li>
-              <li><a href="#">Política de Privacidad</a></li>
-            </ul>
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div>
+          <h4 className="footer-title">Redes Sociales</h4>
+          <div className="footer-icons">
+            <a href="#"><i className="ri-twitter-x-line"></i></a> {/* X (Twitter) Icon */}
+            <a href="#"><i className="ri-youtube-line"></i></a> {/* YouTube Icon */}
+            <a href="#"><i className="ri-github-line"></i></a> {/* Git (GitHub) Icon */}
           </div>
         </div>
-      </footer>
-    );
-  }
-  
-  export default Footer;
-  
+        <div>
+          <h4 className="footer-title">Enlaces Útiles</h4>
+          <ul className="footer-links">
+            <li><a href="#">Soporte</a></li>
+            <li><a href="#">Guía del Juego</a></li>
+          </ul>
+        </div>
+        <div>
+          <h4 className="footer-title">Legal</h4>
+          <ul className="footer-links">
+            <li><a href="#">Términos de Servicio</a></li>
+            <li><a href="#">Política de Privacidad</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,22 @@
 import { Link } from "react-router-dom";
+import React from "react";
 
 function Header() {
   return (
     <header className="header">
       <div className="header-container">
-        <h1 className="logo">Gods of Eternia</h1>
+        <div className="logo-container">
+          <img 
+            src="/public/Logo_1.jpg" 
+            alt="Gods of Eternia Logo" 
+            className="game-logo" 
+          />
+          <h1 className="game-title">Gods of Eternia</h1>
+        </div>
         <nav className="nav">
-          <Link to="/">Inicio</Link>
-          <Link to="/register">Registro</Link>
-          <Link to="/about">Acerca de</Link>
+          <Link to="/" className="nav-link">Inicio</Link>
+          <Link to="/register" className="nav-link">Registro</Link>
+          <Link to="/about" className="nav-link">Acerca de</Link>
         </nav>
       </div>
     </header>
